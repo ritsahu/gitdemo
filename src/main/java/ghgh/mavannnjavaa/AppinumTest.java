74898930498bd333d84ba536b2f06bde1910e03f
ritsahu <ritesh.sahu1104995@gmail.com>
2023-05-16 14:25:42 +0530

    First Commit

---
+++ src/main/java/ghgh/mavannnjavaa/AppinumTest.java
@@ -0,0 +1,18 @@
+package ghgh.mavannnjavaa;
+
+import org.testng.annotations.Test;
+
+public class AppinumTest {
+
+	@Test
+	public void NavigationJira() {
+		
+		System.out.println("NavigationJira");
+	}
+	@Test
+	public void IOSApps() {
+		
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/main/java/ghgh/mavannnjavaa/SeleniumsTest.java
@@ -0,0 +1,21 @@
+package ghgh.mavannnjavaa;
+
+import org.testng.annotations.Test;
+
+public class SeleniumsTest {
+
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+
+	}
+	
+	@Test
+	public void element() {
+		
+		System.out.println("element");
+		
+	}
+	
+	
+}
